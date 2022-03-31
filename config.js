86ffcf5f004fcef8a18afcfe7cb6ca639b7e554f
Rapieree <serega.mironov11@gmail.com>
2022-03-31 23:49:47 +0300

    Add primary server structure

---
+++ config.js
@@ -0,0 +1,9 @@
+require(`dotenv`).config();
+
+const {
+  PORT,
+} = (process || {}).env;
+
+module.exports = {
+  PORT,
+};
--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const express = require(`express`);
+const cors = require(`cors`);
+const cookieParser = require(`cookie-parser`);
+const envs = require(`./config`);
 
+const PORT = envs.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors);
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server listen on ${PORT} port`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
